d166d976a73a5c1b298ea52355a259635d6a9ce0
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2023-06-24 19:20:56 -0500

    initial commit

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name");
+    table.string("last_name");
     table.timestamps(true, true);
   });
 };
