d657da0cae8da10088f514688369dc8bbdefb405
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2023-07-31 19:34:53 -0500

    I have no idea what's going on

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -14,7 +14,6 @@ function isValidNumber(req, res, next){
     }
     next();
   }
-
 // checks if the table name for the new table is at least 2 characters long
 function isValidLength(req, res, next){
     const { data = {} } = req.body;
@@ -23,7 +22,6 @@ function isValidLength(req, res, next){
     }
     next();
 }
-
 // checks if the table being looked up
 async function tableExists(req, res, next) {
   const { table_id } = req.params;
@@ -42,8 +40,6 @@ async function tableExists(req, res, next) {
     return next(error);
   }
 }
-
-
 // validates whether properties are included
 function bodyDataHas(propertyName) {
   return function (req, res, next) {
@@ -54,7 +50,6 @@ function bodyDataHas(propertyName) {
     next({ status: 400, message: `Must include a ${propertyName}` });
   };
 }
-
 // Validator function to check if the table has sufficient capacity
 async function isValidCapacity(req, res, next) {
   const { data = {} } = req.body;
@@ -63,40 +58,30 @@ async function isValidCapacity(req, res, next) {
   const reservation = await reservationsService.read(data.reservation_id)
   const requestedCapacity = reservation.people;
   const tableCapacity = table.capacity;
-
   if (requestedCapacity > tableCapacity) {
     return next({
       status: 400,
       message: `Cannot update reservation. Requested number of people exceeds table capacity.`,
     });
   }
-
   next();
 }
-
-
 // Validator function to check if the table is already occupied
 async function isTableOccupied(req, res, next) {
   const { table_id } = res.locals;
   const table = await service.read(table_id); // Read the table from the database
-
   if (table.reservation_id) {
     return next({
       status: 400,
       message: `Cannot seat reservation. The table is already occupied.`,
     });
-  } else {
-    res.status(200).json({})
   }
-
   next();
 }
-
 // a validator for when the table is already empty
 async function isTableEmpty(req, res, next) {
   const { table_id } = res.locals;
   const table = await service.read(table_id); // Read the table from the database
-
   if (!table || !table.reservation_id) {
     return next({
       status: 400,
@@ -108,27 +93,21 @@ async function isTableEmpty(req, res, next) {
 
   next();
 }
-
-
 // v FETCH FUNCTIONS v
-
 async function list(req, res) {
      res.json({
        data: await service.list(req.query)
      });
    }
-
 async function read(req, res, next) {
   const { table_id } = req.params;
   const table = await service.read(table_id);
-
   if (table) {
     res.json({ data: table });
   } else {
     next({ status: 404, message: `Table with id ${table_id} not found.` });
   }
 }
-
 async function create(req, res) {
     const data = await service.create(req.body.data);
     res.status(201).json({
@@ -146,7 +125,6 @@ async function update(req, res, next) {
 		if (status === 'seated') {
 			throw new Error(`reservation id ${reservation_id} has already been seated`);
 		}
-
 		const data = await service.update({ table_id, reservation_id }); // Pass both table_id and reservation_id to the service
 		await service.updateReservationStatus({ reservation_id, status: 'seated' });
 		
@@ -156,22 +134,18 @@ async function update(req, res, next) {
 		res.status(400).json({ error: err.message})
 	}
 }
-
 async function emptyTable(req, res) {
 	const { table_id } = res.locals;
 	const { reservation_id } = req.body.data; // Get reservation_id and people from the request body
   console.log("*****************", table_id)
 	const data = await service.emptyTable( table_id );
 	await service.updateReservationStatus({ reservation_id, status: 'finished' });
-
   res.status(200).json({})
 }
-
 // v ADDITIONAL VALIDATORS v
  const has_table_name = bodyDataHas("table_name")
  const has_capacity = bodyDataHas("capacity")
  const has_reservation_id = bodyDataHas("reservation_id")
-
    module.exports = {
     list: [asyncErrorBoundary(list)], 
     create: [has_table_name, has_capacity, isValidLength, isValidNumber, asyncErrorBoundary(create)],
